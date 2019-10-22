325774e81d6ca8d32cf7191f39cc2fe83100fd3a
y-yxx <1243175926@qq.com>
2019-10-22 12:55:26 +0800

    work7slide bug fix

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -1,6 +1,6 @@
 function bubbleSort(str) {
     let arr = str.split(',')
-    let sortlog = []
+    let sortLog = []
     for (let i = 0; i < arr.length -1; i++) {
         for (let j = 0; j < arr.length - i - 1; j++) {
             sortLog.push([arr.concat(), [j, j + 1]])
